4892047f8dd9c2bcf97a578f4c51dd7df31b3b16
wenjie <wenjie@zgzqkh.xin>
2017-12-01 18:08:00 +0800

    update web to context

--- src/main/resources/archetype-resources/context/src/main/java/__artifactId__/appservice/impl/UserServiceImpl.java
+++ src/main/resources/archetype-resources/context/src/main/java/__artifactId__/appservice/impl/UserServiceImpl.java
@@ -1,8 +1,8 @@
 #set( $symbol_pound = '#' )
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
-
 package ${package}.${artifactId}.appservice.impl;
+
 import com.jovezhao.nest.starter.AppService;
 import ${package}.${artifactId}.appservice.inter.UserService;
 
--- src/main/resources/archetype-resources/context/src/main/java/__artifactId__/appservice/inter/UserService.java
+++ src/main/resources/archetype-resources/context/src/main/java/__artifactId__/appservice/inter/UserService.java
@@ -1,7 +1,6 @@
 #set( $symbol_pound = '#' )
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
-
 package ${package}.${artifactId}.appservice.inter;
 
 public interface UserService {
