98d6756474bc8a16b6e11e4d373848354c3b3ebe
wenjie <1569925@qq.com>
2018-03-30 15:50:11 +0800

    新增docker脚本和application基本扫描目录

--- src/main/resources/archetype-resources/context/src/main/java/context/Application.java
+++ src/main/resources/archetype-resources/context/src/main/java/context/Application.java
@@ -3,6 +3,8 @@
 #set($symbol_escape='\' )
 package ${package}.context;
 
+import com.jovezhao.nest.starter.EnableNest;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -10,6 +12,12 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableNest
+@MapperScan("${package}.context.appservice.impl.domain.repository.mappers")
+@ComponentScans({
+        @ComponentScan("${package}.context"),
+        @ComponentScan("com.zqkh.common.configuration")
+})
 public class Application {
 
     public static void main(String[] args) {
